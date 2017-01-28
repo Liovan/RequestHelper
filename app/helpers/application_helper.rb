56cfda34d38aeb4094eb8bddf099b3626b74980e
liovan <li0van@yahoo.com>
2017-01-28 17:13:39 +0330

    helpers, gitignore, gaurdfile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  #sets Global app's name
+	$APP_NAME = "درخواست"
+
+  #return full title
+	def full_title(page_title = '')
+		base_title = $APP_NAME
+		if page_title.empty?
+			base_title
+		else
+			"#{page_title} | #{base_title}"
+		end
+	end
+
+  def javascript(*files)
+    content_for(:javascript){javascript_include_tag *files, 'data-turbolinks-track': 'reload' }
+  end
+  def stylesheet(*files)
+    content_for(:stylesheet){stylesheet_link_tag(*files , 'data-turbolinks-track': 'reload')}
+  end
+
 end
