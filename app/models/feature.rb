3f386324b740db3d05028de9e95044268dcde413
Mahdi <mahdializadeh20@gmail.com>
2017-03-20 01:19:44 +0330

    remove join table feature and place

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,5 +1,4 @@
 class Feature < ApplicationRecord
-  has_and_belongs_to_many :places
   has_many :requests
   has_many :needs
   validates :name,presence: true,length: {in:1..45,message: "can't be nil"}
