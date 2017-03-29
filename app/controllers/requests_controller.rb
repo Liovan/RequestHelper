7091bb6d9264e2c5757af9523c55aa02973b612e
Mahdi <mahdializadeh20@gmail.com>
2017-03-30 01:29:48 +0430

    fix request checkbox for result students

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -23,20 +23,27 @@ class RequestsController < ApplicationController
     feature=Feature.find(params[:feature_id])
     first_confirm=get_module_routes(feature.id).first # get first value in module routes helper-]
     status=1
+
         if !feature.nil?
           valid=false
           feature.needs.map do |need|
-            if params[:"form_#{need.id}"].present?
+
+            if !params[:"form_#{need.id}"].present? && need.input_type==3 || params[:"form_#{need.id}"].present? && need.input_type!=3
                 # validation for dynamic result student
                 # types_need => 1: number , 2: character , 3: check box (true ot false)
+              unless params[:"form_#{need.id}"].present?
+                params[:"form_#{need.id}"]=false
+              end
+
                   case need.input_type
                     when types_need.key("عدد")
                       params[:"form_#{need.id}"].match(/\A[+-]?\d+?(\.\d+)?\Z/)!=nil ? valid=true : valid=false
                     when types_need.key("کارکتر")
                       params[:"form_#{need.id}"].match(/\A[اآبپتثئجچحخدذرزژسشصضطظعغفقکگلمنوهیءأؤّ\s]+\z/)!=nil ? valid=true : valid=false
                     when types_need.key("گزینه")
-                      params[:"form_#{need.id}"]==true || params[:"form_#{need.id}"]==false || params[:"form_#{need.id}"]==nil ? valid=true : valid=false
+                      params[:"form_#{need.id}"]==true || params[:"form_#{need.id}"]==false ? valid=true : valid=false
                   end
+
                    # if it was valid go for save data
                 if valid==true
                   Request.transaction do
@@ -58,6 +65,8 @@ class RequestsController < ApplicationController
               redirect_to students_path,danger:"ارسال درخواست لغو شد"
               break # break in loop needs of feature
             end
+
+
           end
         else
            # if not fimd feature in params[:featue_id]
