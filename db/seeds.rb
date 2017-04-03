089c6a1ef2e3d7f135a724e12be198c5701b7aff
Mahdi <mahdializadeh20@gmail.com>
2017-04-03 14:08:32 +0430

    Added seed for owner user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Staff.create(f_name: "میم", l_name: "میم زاده", username: "admin_meam", password: "meam_group_admin2121")
