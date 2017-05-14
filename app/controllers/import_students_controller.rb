3f0bdc7019ba9753c4f6d1cd5421608eb7459f8d
mhi20 <mahdializadeh20@gmail.com>
2017-05-15 02:44:34 +0430

    added file upload to import_student but not complete sync between temp and students

--- app/controllers/import_students_controller.rb
+++ app/controllers/import_students_controller.rb
@@ -16,13 +16,55 @@ class ImportStudentsController < ApplicationController
     if params[:id].present?
       attach=Attachment.find params[:id]
 
-      file_location =   request.base_url+attach.files.url
-      open(file_location, 'r:utf-8') do |f|   # don't forget to specify the UTF-8 encoding!!
+      file_location =request.base_url+attach.files.url
+      is_error=false
+      open(file_location, 'r:utf-8') do |f|  
         data = SmarterCSV.process(f)
-        sdfsdfds
+          data.each do |student|
+            if is_error
+              flash[:danger]="متاسفانه در هنگام بهنگام سازی دچار خطا شده است ، لطفاْ با راهبر تماس بگیرید"
+              redirect_to import_students_path
+              return 0
+            end
+            if student[:major].strip=='نرم افزار' && student[:grade].strip=='کاردانی'
+                field_of_student=1
+                elsif  student[:major].strip=='حسابداری' && student[:grade].strip=='کاردانی'
+                field_of_student=2
+                elsif student[:major].strip=='ICT' && student[:grade].strip=='کاردانی'
+                field_of_student=3
+                elsif student[:major].strip=='الکترونیک' && student[:grade].strip=='کاردانی'
+                field_of_student=4
+                elsif student[:major].strip=='IT' && student[:grade].strip=='کاردانی'
+                field_of_student=5
+                
+                elsif student[:major].strip=='نرم افزار' && student[:grade].strip=='کارشناسی'
+                field_of_student=11
+                elsif student[:major].strip=='حسابداری' && student[:grade].strip=='کارشناسی'
+                field_of_student=12
+                elsif student[:major].strip=='ICT' && student[:grade].strip=='کارشناسی'
+                field_of_student=13
+                elsif student[:major].strip=='الکترونیک' && student[:grade].strip=='کارشناسی'
+                field_of_student=14
+                elsif student[:major].strip=='IT' && student[:grade].strip=='کارشناسی'
+                field_of_student=15
+            end
+            
+            Temp.transaction do
+               if  Temp.create(f_name:student[:f_name],l_name:student[:l_name],
+                            father_name:student[:father_name],meli_code:student[:meli_code],
+                            city:student[:city],field:field_of_student,student_code:student[:student_code],password_digest:BCrypt::Password.create(student[:meli_code]))
+                 is_error=false
+               else
+                 is_error=true
+                 raise ActiveRecord::Rollback
+               end
+            end
+          end
+        
+        #todo add sync temp and students 
+        
+        
       end
-      
-      tt4444
     end
   end
 
